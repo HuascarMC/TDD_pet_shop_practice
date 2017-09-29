15ec849d86dbe3cdb802885d8e8221fdef6d390d
Huascar <huascarmc@hotmail.com>
2017-09-29 13:26:21 +0100

    can tell number of pets per costumer

--- pet_shop.rb
+++ pet_shop.rb
@@ -56,3 +56,7 @@ def add_pet_to_stock(pet_shop, new_pet)
   pet_shop[:pets] << new_pet
   return pet_shop[:pets].length
 end
+
+def customer_pet_count(customer)
+  return customer[:pets].length
+end
